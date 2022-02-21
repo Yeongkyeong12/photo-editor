838b5a10509caec4d1eb59683514e617c0fc61ec
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-21 23:20:53 +0900

    Feat: add Default_options

--- src/Slider.js
+++ src/Slider.js
@@ -1,11 +1,11 @@
-// import React from "react";
+import React from "react";
 
-// export default function Slider() {
-//   return (
-//     <div className="slider-container">
-//       <input type="range" className="slider" />
-//     </div>
-//   );
-// }
+export default function Slider() {
+  return (
+    <div className="slider-container">
+      <input type="range" className="slider" />
+    </div>
+  );
+}
 
 <script></script>;
